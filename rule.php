940cb2a203c817fd80b0838fcb407e3698a41418
Vaneyck Seme Otoki <vaneycksemeotoki@gmail.com>
2013-02-06 21:31:41 +0300

    added shells for code

---
+++ rule.php
@@ -0,0 +1,11 @@
+<?php
+class Rule {
+	function testContent($content_id, $rule_id) {
+
+	}
+
+	function testAllRules($content_id) {
+
+	}
+}
+?>
---
+++ violation.php
@@ -0,0 +1,15 @@
+<?php
+class Violation {
+	function addViolation($content_id, $rule_id, $text_violated) {
+
+	}
+	
+	function editSource($sourceId, $type, $sourceReference, $content) {
+
+	}
+
+	function removeSource($source_id) {
+
+	}
+}
+?>
