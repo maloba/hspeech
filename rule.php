37a7d7a6eba41a17f2835b4e46575c7e03a350c1
Vaneyck Seme Otoki <vaneycksemeotoki@gmail.com>
2013-02-06 21:35:42 +0300

    added restrictions so that scripts cannot be accessed directly

--- rule.php
+++ rule.php
@@ -1,4 +1,4 @@
-<?php
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 class Rule {
 	function testContent($content_id, $rule_id) {
 
--- violation.php
+++ violation.php
@@ -1,9 +1,9 @@
-<?php
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 class Violation {
 	function addViolation($content_id, $rule_id, $text_violated) {
 
 	}
-	
+
 	function editSource($sourceId, $type, $sourceReference, $content) {
 
 	}
