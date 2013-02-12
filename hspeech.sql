ee507d69aa64629765b7f3c2d5686b7c85bc4366
Vaneyck Seme Otoki <vaneycksemeotoki@gmail.com>
2013-02-12 22:37:12 +0300

    added new database file

--- hspeech.sql
+++ hspeech.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 06, 2013 at 06:54 PM
+-- Generation Time: Feb 12, 2013 at 07:36 PM
 -- Server version: 5.5.24-log
 -- PHP Version: 5.3.13
 
@@ -31,7 +31,14 @@ CREATE TABLE IF NOT EXISTS `rules` (
   `ruleAsRegex` text NOT NULL,
   `descriptionOfRule` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `rules`
+--
+
+INSERT INTO `rules` (`id`, `ruleAsRegex`, `descriptionOfRule`) VALUES
+(1, '/.*kibaki.*/', 'Should not have kibaki in it');
 
 -- --------------------------------------------------------
 
@@ -45,7 +52,14 @@ CREATE TABLE IF NOT EXISTS `source` (
   `sourceReference` text NOT NULL,
   `content` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `source`
+--
+
+INSERT INTO `source` (`id`, `type`, `sourceReference`, `content`) VALUES
+(1, 'sms', '123123123', 'President kibaki yesterday expressed concern over reports that some Kenyans are using social media to spread hate speech ahead of the March 4 polls.\r\n“I call kibaki the young people to rise up in unity and reject all perpetrators of hate and division,” said the President.');
 
 -- --------------------------------------------------------
 
@@ -59,7 +73,16 @@ CREATE TABLE IF NOT EXISTS `violations` (
   `sourceId` int(11) NOT NULL,
   `textWithViolation` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `violations`
+--
+
+INSERT INTO `violations` (`id`, `ruleId`, `sourceId`, `textWithViolation`) VALUES
+(2, 1, 1, 'President kibaki yesterday expressed concern over reports that some Kenyans are using social media to spread hate speech ahead of the March 4 polls.\r'),
+(3, 1, 1, 'President kibaki yesterday expressed concern over reports that some Kenyans are using social media to spread hate speech ahead of the March 4 polls.\r'),
+(4, 1, 1, '“I call kibaki the young people to rise up in unity and reject all perpetrators of hate and division,” said the President.');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
